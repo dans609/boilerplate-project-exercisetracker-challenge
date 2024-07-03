849b4efba7e7854907bdd9eac243dfd6613f5672
Dandi Anastasa <dandianastasa12@gmail.com>
2024-07-03 16:20:28 +0700

    prepare all important variable/pkg/middleware for later use

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
