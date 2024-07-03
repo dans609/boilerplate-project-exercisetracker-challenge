b2c313775fc6fdf0f0417712fdcebab4b98b3e84
Dandi Anastasa <dandianastasa12@gmail.com>
2024-07-04 00:19:33 +0700

    configure mongoose to connect to MongoDB Atlas

--- index.js
+++ index.js
@@ -6,19 +6,42 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 require('dotenv').config()
 
+// constant config
+const INVALID_USERNAME = 'Invalid username'
+const CONNECTED_TO_HOST = 'Connected!'
+const FAIL_TO_CONNECT = 'Error: Failed to connect to the given hostname'
+
+// middleware config
 app.use(cors())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+// mongoose config
+const URI = process.env.MONGO_URI
+mongoose.connect(URI, {dbName: process.env.DB_NAME})
+  .then(() => console.log(CONNECTED_TO_HOST))
+  .catch((err) => {
+    console.log(FAIL_TO_CONNECT)
+    console.log(`reason: ${err.syscall} ${err.code}`)
+  })
+
+// routes
 app.get('/', (_req, res) => {
   res.sendFile(__dirname + '/views/index.html')
-});
+})
 
+app.route('/api/users')
+  .post((req, res) => {
+    const {username} = req.body
+    if(username === undefined || username === "")
+      return res.json({error: INVALID_USERNAME})
 
+    console.log(username)
+  });
 
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
