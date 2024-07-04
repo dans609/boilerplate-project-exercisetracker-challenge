e614b15bdb30eb685fe67d15802d9561b374c5a3
Dandi Anastasa <dandianastasa12@gmail.com>
2024-07-04 22:26:41 +0700

    fix: error when trying to access username property on user schema `userSchema.username`

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose')
-const {userSchema} = require('./User.js')
 const {Schema} = mongoose
 
 const exerciseSchema = new Schema({
-    username: userSchema.username,
+    username: {type: String, required: true},
     description: {type: String, required: true},
     duration: {type: Number, min: 1, required: true},
     date: {type: Date, default: Date.now},
     _userId: Schema.Types.ObjectId
 })
-const Exercise = mongoose.model('Exercise', exerciseSchema)
 
-exports.exerciseSchema = exerciseSchema
-exports.ExerciseModel = Exercise
+exports.ExerciseModel = mongoose.model('Exercise', exerciseSchema)
